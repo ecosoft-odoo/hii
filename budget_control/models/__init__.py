345de7d446049896ac24f2be49f0be8cc5ce3117
Kitti U <kittiu@gmail.com>
2019-11-05 09:28:48 +0700

    Add initial budget_control addons

---
+++ budget_control/models/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2019 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import account_move
+from . import account_invoice
+from . import budget_period
+from . import budget_control
+from . import mis_budget_item
+from . import base_budget_move
+from . import budget_move_forward
