7bf9e485470db545640fa2439f56eb7a1f673272
Kitti U <kittiu@gmail.com>
2019-11-05 10:22:29 +0700

    Add hii module

--- hii/__manifest__.py
+++ hii/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Sale Order Type",
+    "name": "HII Customization",
     "version": "12.0.1.0.0",
     "category": "Hydro Informatics Institute",
     "author": "Ecosoft,Odoo Community Association (OCA)",
@@ -15,16 +15,16 @@
         'budget_control_expense',
         'budget_control_purchase',
         # 'budget_control_purchase_request',
-        'analytic_tag_dimension_enhanced',
+        # 'analytic_tag_dimension_enhanced',
         'budget_control_tag_dimension',
         'budget_control_expense_tag_dimension',
         'budget_control_purchase_tag_dimension',
         'budget_control_tier_validation',
     ],
     "data": [
-        'security/ir.model.access.csv',
-        'security/dimension_security.xml',
-        'views/dimension_view.xml',
+        'budget_dimension/security/ir.model.access.csv',
+        'budget_dimension/security/dimension_security.xml',
+        'budget_dimension/views/dimension_view.xml',
     ],
     'installable': True,
 }
